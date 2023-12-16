0b0d3f513e036af742465db515a9f467d310aee5
Carlos Grillet <carlosbeta5000@gmail.com>
2023-12-16 22:32:01 +0100

    fix: the repo can now dynamically add backups

--- script/db_restore.sh
+++ script/db_restore.sh
@@ -3,6 +3,20 @@
 echo "Creating backup directory..."
 mkdir -p /var/opt/mssql/init
 
+echo "Geting mdf and ldf files..."
+MDF=$(/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $(echo $MSSQL_SA_PASSWORD) \
+  -Q 'RESTORE FILELISTONLY FROM DISK = "/var/opt/mssql/backup/'$(echo $MSSQL_DB_NAME)'.bak"' \
+  | tr -s ' ' | cut -d ' ' -f 1-2 | grep '.mdf')
+
+LDF=$(/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $(echo $MSSQL_SA_PASSWORD) \
+  -Q 'RESTORE FILELISTONLY FROM DISK = "/var/opt/mssql/backup/'$(echo $MSSQL_DB_NAME)'.bak"' \
+  | tr -s ' ' | cut -d ' ' -f 1-2 | grep '.ldf')
+
+MDF_NAME=$(echo $MDF | awk 'NR==1{print $1}')
+MDF_FILE=$(echo $MDF | awk 'NR==1{print $2}' | grep -oP '(?<=\\)[^\\]+$')
+LDF_NAME=$(echo $LDF | awk 'NR==1{print $1}')
+LDF_FILE=$(echo $LDF | awk 'NR==1{print $2}' | grep -oP '(?<=\\)[^\\]+$')
+
 echo "Restoring database $(echo $MSSQL_DB_NAME)..."
 cat <<EOF > /var/opt/mssql/init/setup.sql
 USE master;
@@ -10,8 +24,8 @@ USE master;
 RESTORE DATABASE $(echo $MSSQL_DB_NAME)
 FROM DISK = '/var/opt/mssql/backup/$(echo $MSSQL_DB_NAME).bak'
 WITH
-    MOVE '$(echo $MSSQL_DB_NAME)' TO '/var/opt/mssql/data/$(echo $MSSQL_DB_NAME).mdf',
-    MOVE '$(echo $MSSQL_DB_NAME)_log' TO '/var/opt/mssql/data/$(echo $MSSQL_DB_NAME)_log.ldf',
+    MOVE '$MDF_NAME' TO '/var/opt/mssql/data/$MDF_FILE',
+    MOVE '$LDF_NAME' TO '/var/opt/mssql/data/$LDF_FILE',
     STATS = 5;
 
 ALTER DATABASE $(echo $MSSQL_DB_NAME) SET ONLINE;
